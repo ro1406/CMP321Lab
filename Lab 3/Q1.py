f231f95acdd519e3e8ed63b12ff0291bcaae0822
Rohan <rohanmitra8@gmail.com>
2022-06-13 10:26:59 +0400

    Q1 half done

--- Lab 3/Q1.py
+++ Lab 3/Q1.py
@@ -5,4 +5,19 @@ Created on Mon Jun 13 10:17:28 2022
 @author: rohan
 """
 
-#part a
+#part a)
+
+def fib(n):
+    if n<=1:
+        return 1
+    return fib(n-1)+fib(n-2)
+
+print([fib(i) for i in range(11)])
+
+#part b)
+def fibAcc(n,prev=0,acc=1):
+    if n<=0:
+        return acc
+    return fibAcc(n-1,acc,prev+acc)
+
+print([fibAcc(i) for i in range(11)])
