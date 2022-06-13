86f188e3e61752365e7ec1b8bce0ea4779ff43ff
Rohan <rohanmitra8@gmail.com>
2022-06-13 13:56:37 +0400

    lab3 finished

--- Lab 3/Q5.py
+++ Lab 3/Q5.py
@@ -9,11 +9,15 @@ Created on Mon Jun 13 12:46:36 2022
 from math import sqrt
 
 def apply_all(func, lst):
-    return list((func(i) for i in lst))
+    return [func(i) for i in lst]
 
 l1 = [4, 9, 16, 25]
 print(apply_all(sqrt, l1))
 
 #(b)
-
-print(apply_all(lambda lst:str(lst).isdigit(), ["1234", "hellow","class23"]))
+li=["1234", "hellow","class23"]
+ans=apply_all(lambda lst:str(lst).isdigit(), li)
+print("The elements that contain only numbers are:")
+for string,x in zip(li,ans):
+    if x:
+        print(string)
