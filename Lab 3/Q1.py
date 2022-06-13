a92df264d0910ac2b85aea1840c62a56bddc3434
Rohan <rohanmitra8@gmail.com>
2022-06-13 12:49:19 +0400

    Update Q1.py

--- Lab 3/Q1.py
+++ Lab 3/Q1.py
@@ -17,10 +17,10 @@ print([fib(i) for i in range(11)])
 #part b)
 
 def fibAcc(n):
-    def fibAccPrivate(n,prev=0,acc=1):
+    def fibAcc(n,prev=0,acc=1):
         if n<=0:
             return acc
-        return fibAccPrivate(n-1,acc,prev+acc)
-    return fibAccPrivate(n)
+        return fibAcc(n-1,acc,prev+acc)
+    return fibAcc(n)
 
 print([fibAcc(i) for i in range(11)])
