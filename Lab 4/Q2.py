85e70ede6874380e4dcdb5dc585b00fd2d0cb8d0
Rohan <rohanmitra8@gmail.com>
2022-06-15 11:49:48 +0400

    Q2 complete

--- Lab 4/Q2.py
+++ Lab 4/Q2.py
@@ -25,7 +25,7 @@ class Employee:
     def __gt__(self,other):
          return (self.id > other.id)
 
-def sort(values, comp=lambda x,y:x.id>y.id):
+def sort(values, comp=Employee.__gt__):
     for i in range(1, len(values)):
         for j in range(0, len(values)-1):
             if (comp(values[j],values[j+1])):#if(values[j] > values[j+1]):
