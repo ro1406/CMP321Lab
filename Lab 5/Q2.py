9abbc46cbcb8fa19d74a3e565e185aede71782ff
keshavramesh23 <keshavramesh2003@gmail.com>
2022-06-20 14:13:06 +0400

    Lab done

--- Lab 5/Q2.py
+++ Lab 5/Q2.py
@@ -9,9 +9,8 @@ from functools import reduce
 
 def selectionSort(arr):
     for i in range(len(arr)-1):
-        minIndex = i + arr[i:].index(reduce(lambda x,y : x if x < y else y, arr[i::]))
-        if(minIndex != i):
-            arr[i], arr[minIndex] = arr[minIndex], arr[i]
+        minIndex = i + arr[i:].index(reduce(lambda x,y : x if x < y else y,arr[i:]))
+        if(minIndex != i): arr[i], arr[minIndex] = arr[minIndex], arr[i]
 
 arr = [-3, 5, 2, 8, 2, 9, 0, -7]
 print("Unsorted Array: ",arr)	
