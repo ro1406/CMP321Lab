2519d3246f84c553856f27c10a3d8468d82908b6
keshavramesh23 <keshavramesh2003@gmail.com>
2022-06-27 11:24:27 +0400

    started lab 2 + minor changes to doc

---
+++ Lab 7/Q2.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 27 11:12:41 2022
+
+@author: keshav
+"""
+
+"""
+Write a Python script that uses a regular expression to check if a given string represents a valid assignment expression, or not, as defined by the following specifications (grammar rules). Examples are included hereafter.
+
+(1) An assignment expression is:
+– An identifier, followed by the equal sign ('='), followed by either of an identifier or a number 
+– optionally followed by one or more of : an operator followed by either an identifier or a number.
+
+(2) An identifier is a letter, optionally followed by one or more of:  a letter or a digit.
+
+(3) A number may optionally start with either the plus sign ('+') or minus sign ('–'). It consists of one or more digits, optionally followed by: a decimal point ('.') and one or more digits.
+
+(4) An operator is either of the symbols for addition ('+'), subtraction ('–'), multiplication ('*'), division ('/'), or modulus ('%').
+
+(5) A letter is any character from the small or uppercase alphabets: 'a-z' or 'A-Z'.
+
+(6) A digit is any from 0 to 9.
+
+"""
+
+import re
+
+def checkExpression(expression):
+    if re.fullmatch(r"{0}\s?[=]")
+
+lst = ["x = y", "y = y * z", "z = a + b - 1 / c", "p = 3.1415 * d", "x = y *","y = y z","z = + b – 1 /c","3 = p * d"] #test cases for checking expression
+
+for item in lst:
+    checkExpression(item)
